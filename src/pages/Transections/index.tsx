ff22c0b3c0b86efa10c9d4641a663db317b97b65
Lucas Lessa Pacheco <lucas.pacheco@box360.net.br>
2023-03-03 15:37:11 -0300

    criando tabela de registro de custos e entradas

--- src/pages/Transections/index.tsx
+++ src/pages/Transections/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { TransectionsContainer, TransectionsTable, PriceHighLight } from './styles'
 
 export function Transections() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransectionsContainer>
+        <TransectionsTable>
+          <tbody>
+
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant='income'>
+                R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>Hamurguer</td>
+              <td>
+                <PriceHighLight variant='outcome'>
+                - R$ 59,00
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+          </tbody>
+        </TransectionsTable>
+      </TransectionsContainer>
     </div>
   )
 }
