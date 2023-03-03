46f889c9833896a1bd8673aa87ed17ca7e61cf5b
Lucas Lessa Pacheco <lucas.pacheco@box360.net.br>
2023-03-03 14:55:15 -0300

    concluindo header

--- src/pages/Transections/index.tsx
+++ src/pages/Transections/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 export function Transections() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
