b42c840d241cd01112d7faec476f873fa60033d4
Lucas Lessa Pacheco <lucas.pacheco@box360.net.br>
2023-03-03 16:18:35 -0300

    search form concluido

--- src/pages/Transections/index.tsx
+++ src/pages/Transections/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 import { TransectionsContainer, TransectionsTable, PriceHighLight } from './styles'
 
 export function Transections() {
@@ -10,6 +11,8 @@ export function Transections() {
       <Summary />
 
       <TransectionsContainer>
+        <SearchForm />
+        
         <TransectionsTable>
           <tbody>
 
